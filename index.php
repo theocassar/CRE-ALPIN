3253d54eccb6b714d61e907e51f39f2bc13e3d52
Théo <59197790+theocassar@users.noreply.github.com>
2021-06-27 22:19:53 +0200

    🔧

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="description" content="Free Web tutorials">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>CRE'ALPIN | Une randonnée pour préserver le Mont Blanc</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
