dd7ee713b52e910a31f5ba29ba149c38cad12bc2
Théo <59197790+theocassar@users.noreply.github.com>
2021-07-19 20:56:09 +0200

    fix css

--- assets/php/mon-compte.php
+++ assets/php/mon-compte.php
@@ -27,7 +27,28 @@
   <?php include '../../CRE-ALPIN/assets/php/header.php' ?>
 
   <main>
-    
+    <div class="logo-container">
+      <a href="#"><img src="../../src/img/logo-couleur-vectorisé.svg" alt="Logo CRE'LPIN"></a>
+    </div>
+
+    <form action="verification.php" method="POST">
+      <h1>Connexion</h1>
+
+      <label><b>Nom d'utilisateur</b></label>
+      <input type="text" placeholder="Entrer le nom d'utilisateur" name="username" required>
+
+      <label><b>Mot de passe</b></label>
+      <input type="password" placeholder="Entrer le mot de passe" name="password" required>
+
+      <input type="submit" id='submit' value='LOGIN'>
+      <?php
+      if (isset($_GET['erreur'])) {
+        $err = $_GET['erreur'];
+        if ($err == 1 || $err == 2)
+          echo "<p style='color:red'>Utilisateur ou mot de passe incorrect</p>";
+      }
+      ?>
+    </form>
   </main>
 
   <script src="../../assets/js/main.js" defer></script>
